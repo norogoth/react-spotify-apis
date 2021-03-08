b544e4545d76b40d4a4c3348761d3d64e6705e29
Landon Aaker <norogoth@gmail.com>
2021-03-08 13:22:23 -0800

    another commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
 import reactDom from 'react-dom';
 
+const token = "BQB2j85HfD91v4tKyd3CfuLyRt5WyqnqLLndDBwmpYRbCRRCcMxWOxQ_wr4AFXXwnVtT5NaIKdv59cSllRl3M4kc7ow9dfn-caH1xqSXVv97COzWceqys13a99rm6XfDhqasQuqnAdyv";
+const url = "https://api.spotify.com/v1/search";
+
 function App() {
   const [albumList, setAlbumList] = useState([]);
-  
+ 
+  function search(searchString) {
+    console.log("search was called");
+    //run API
+  }
+
+  async function handleClick() {
+    let promise = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    if (promise.ok) {
+      let json = await promise.json();
+    } else {
+      console.log(promise);
+      alert(`HTTP Error: ${promise.status}`);
+    }
+  }
+
   return (
     <div id="mainContainer">
       <h id="mainHeading">Get an Artist's Albums</h>
       <input id="artistSearch"></input>
+      <button onClick={handleClick}>search</button>
       <ul id="albumList">{albumList}</ul>
     </div>
 

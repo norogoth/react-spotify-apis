794deb433876858a9e34bda01d8a9c141e39ff03
Landon Aaker <norogoth@gmail.com>
2021-03-31 16:14:13 -0700

    Committing my front end to communicate with my muf BE

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import reactDom from 'react-dom';
 
 const clientId = "6cb2e9a6a5d24292a94d3a07f2c9ad2b";
@@ -8,6 +8,61 @@ const tokenUrl = "https://accounts.spotify.com/api/token";
 const searchUrl = "https://api.spotify.com/v1/search";
 let token = "null";
 
+function Muffins() {
+  const [muffinList, setMuffinList] = useState([]);
+  const muffinUrl = `http://127.0.0.1:8000/test`;
+
+  async function getMuffins() {
+    return fetch(muffinUrl, {
+      method: 'GET',
+    }).then(function (response) {
+      return response.json();
+    }).then (function (data) {
+      //console.log("data: ",data["data"]);
+      let newMuffinList = [];
+      for (const [key, value] of Object.entries(data["data"])){
+        //console.log("*",value);
+        const row = document.createElement('tr');
+        const name = document.createElement('td');
+        name.innerHTML = data["data"]["name"];
+        const desc = document.createElement('td');
+        desc.innerHTML = data["data"]["description"];
+        row.appendChild(name);
+        row.appendChild(desc);
+        newMuffinList.push(row);
+      }
+      setMuffinList(newMuffinList);
+      console.log("muffinlist",muffinList);
+      return data["data"];
+    });
+  }
+
+  useEffect(() => {
+    getMuffins()
+  }, [])
+
+  return(
+    <div>
+      <h1>Muffins</h1>
+      <table id="muffinTable">
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Descrition</th>
+          </tr>
+          {muffinList.map(muffin => {
+            return (
+              <tr key = {muffin["id"]}>
+                <td>{muffin["name"]}</td>
+                <td>{muffin["description"]}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
 function App() {
   const [albumList, setAlbumList] = useState([]);
@@ -31,6 +86,7 @@ function App() {
       });
   }
 
+
   function searchApi() {
     const searchBox = window.document.getElementById("searchBox");
     const searchString = searchBox.value;
@@ -53,11 +109,12 @@ function App() {
 
   return (
     <div id="mainContainer">
-      <h id="mainHeading">Get an Artist's Albums</h>
+      <h2 id="mainHeading">Get an Artist's Albums</h2>
       <input id="searchBox"></input>
       <button onClick={handleClick}>search</button>
       <button onClick={testClick}>test</button>
       <ul id="albumList">{albumList}</ul>
+      <Muffins/>
     </div>
   );
 }
